f678f9d6ac1fbc2c97e4526b6a03d3cfe513a1e5
Brian Lopez <lopezrocha3531@gmail.com>
2026-06-10 11:45:04 -0700

    Added main and Hello world program

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,7 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-//print hello world
+//
+// print hello world
     System.out.print("Hello World!");
 
 }
